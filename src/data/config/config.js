d9d4dfee453e20aa27c28366c3431abdf1b56cfc
segural <luciano.g.segura@gmail.com>
2021-06-17 16:19:41 -0300

    cambios para deploy

--- src/data/config/config.js
+++ src/data/config/config.js
@@ -1,9 +1,9 @@
 module.exports = {
     "development": {
-      "username": "root",
-      "password": 'root',
-      "database": "casa_digital_db",
-      "host": "127.0.0.1",
+      "username": process.env.DB_USER,
+      "password": process.env.DB_PASS,
+      "database": process.env.DB_NAME,
+      "host": process.env.DB_HOST,
       "dialect": "mysql"
     },
     "test": {
@@ -14,10 +14,10 @@ module.exports = {
       "dialect": "mysql"
     },
     "production": {
-      "username": "root",
-      "password": null,
-      "database": "database_production",
-      "host": "127.0.0.1",
+      "username": process.env.DB_USER,
+      "password": process.env.DB_PASS,
+      "database": process.env.DB_NAME,
+      "host": process.env.DB_HOST,
       "dialect": "mysql"
     }
   }
